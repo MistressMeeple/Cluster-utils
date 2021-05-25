459893460a4d839a4b28e838ba2060178367fdfc
Mistress_Meeple <jskynol001@gmail.com>
2021-05-25 20:08:03 +0100

    Update cluster.sh

--- cluster.sh
+++ cluster.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# ClusterCTRL base dir
+ClusterBase=/var/lib/clusterctrl/
+
+# NFS Base dir
+NFSBase=$ClusterBase/NFS
+# NFS Name scheme: /var/lib/clusterctrl/nfs/?
+NFSName=/pX
+
+# Overlay FS base dir
+OFSBase=$ClusterBase/OFS
+# OFS Name scheme: /var/lib/clusterctrl/ofs/?
+NFSName=/node.X
+
+# Size limit file base dir
+SLBase=$ClusterBase/SL
+# OFS Name scheme: /var/lib/clusterctrl/sl/?
+SLName=/node.X#.ext4
+
+
 function logs(){
     tail -f /var/log/daemon.log /var/log/kern.log
 }
@@ -35,6 +54,14 @@ function setup(){
     for N in 1 2 3 4; do
         ssh pi@p$N "echo 'do the setups'";
     done
+}
+
+function resize_size_limits(){
+    # shutdown nodeN
+    # umount $OFSBASE/$OFSName dir
+    # e2fsck -f /var/virtual_disks/directory_with_size_limit.ext3
+    # resize2fs -p /var/virtual_disks/directory_with_size_limit.ext3 NEW_SIZE
+    # mount -o loop,rw,usrquota,grpquota /var/virtual_disks/directory_with_size_limit.ext3 /path/of/mount/point
 }
 
 function status(){
